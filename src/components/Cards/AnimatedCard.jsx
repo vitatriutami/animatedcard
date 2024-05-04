c0ff092398e2ffcd354bd6af43807d8ea8fafabb
vitatriutami <vitatriutami@gmail.com>
2024-05-05 03:26:54 +0700

    adding card and animation

--- src/components/Cards/AnimatedCard.jsx
+++ src/components/Cards/AnimatedCard.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
-
+import Photo1 from '../../assets/pic/Photo1.png'
+import Photo2 from '../../assets/pic/Photo2.png'
+import Photo3 from '../../assets/pic/Photo3.jpg'
+import { Fade, Slide } from 'react-awesome-reveal'
 
 const CardsData = [
   {
@@ -30,7 +33,35 @@ const AnimatedCard = () => {
       </h1>
 
       {/* cards section */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 place-items-center">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 place-items-center gap-6">
+        { CardsData.map(({id, img, title, desc}) => {
+          return(
+            <div key={id} className='text-white shadow-md rounded-lg overflow-hidden relative group'>
+              <img src={img} alt=''
+              className='w-full max-w-[350px] h-[350px] rounded-lg' />
+
+              {/* overlay section */}
+              <div className='absolute left-0 top-[-100%] opacity-0 group-hover:opacity-100 group-hover:top-[0] p-4 w-full h-full bg-black/60 group-hover:backdrop-blur-sm duration-500'>
+                <div className='space-y-4'>
+                  <Slide cascade>
+                  <h1 className='text-3xl font-bold'>{title}</h1>
+                  <Fade cascade damping={0.05}>
+                    {desc}
+                  </Fade>
+                  <div>
+                    <button className='border border-white px-4 py-2 rounded-lg hover:bg-black/20 duration-300'>
+                      View
+                    </button>
+                  </div>
+                  </Slide>
+                </div>
+              </div>
+
+            </div>
+
+          )
+        })
+        }
 
       </div>
     </div>
